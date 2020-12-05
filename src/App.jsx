1e9c2aa5fc06613571bc841472b36776d2bf2928
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-05 21:15:35 +0530

    project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React Firebase Authentication</div>;
+  return (
+    <div className="App">
+      <h2 className="app__title">
+        <span className="app__react">React</span> &{" "}
+        <span className="app__fire">Firebase</span>{" "}
+        <span className="app__auth">Authentication</span> -{" "}
+        <span className="app__kyle">WebDevSimplified_Kyle</span> |{" "}
+        <span className="app__vin">Vinothkumar S</span>
+        <hr className="app__hr"></hr>
+      </h2>
+    </div>
+  );
 }
 
 export default App;
